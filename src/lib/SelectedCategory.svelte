f8f1c665d1141a76817f323bdc1c39919cd52574
Delwar Ahmed <delwar.habib@gmail.com>
2023-02-11 02:54:44 +0600

    fixed netlify deployment issue

--- src/lib/SelectedCategory.svelte
+++ src/lib/SelectedCategory.svelte
@@ -15,7 +15,7 @@
         <div class="flex justify-center content-center text-center">
             <div class="py-4 text-xl md:lg:text-4xl text-lime-700"><span class="text-bold text-pink-600 underline">{meals.length}</span> meals in this category.</div>
         </div>
-        <div class=" justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 py-6 gap-2 ">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 py-6 gap-2 ">
                 {#each meals as meal}
                     <a href={`/meal/${meal.idMeal}`} data-sveltekit-preload-data="hover">
                         <div class="card card-compact w-full h-full bg-base-100 shadow-xl">
